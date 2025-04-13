c36a749738c5675c7c6a02cb146abc608d1994be
sumanta <sumantajashh@gmail.com>
2025-04-13 20:31:07 +0530

    Generated Project using Spring Initializer

---
+++ BACKEND/HackFest/HackFest/src/main/java/com/sumanta/HackFest/HackFestApplication.java
@@ -0,0 +1,13 @@
+package com.sumanta.HackFest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackFestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackFestApplication.class, args);
+	}
+
+}
