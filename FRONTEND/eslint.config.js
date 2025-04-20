d09b68098d6095c735d8dce89d67dddb3c8c76f8
DIPANU HAZRA <dipanuhazra@gmail.com>
2025-04-20 12:02:04 +0530

    Merge Conflicts Resolved

--- FRONTEND/eslint.config.js
+++ FRONTEND/eslint.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
@@ -28,38 +27,36 @@ export default tseslint.config(
     },
   }
 );
-=======
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+// import js from '@eslint/js'
+// import globals from 'globals'
+// import reactHooks from 'eslint-plugin-react-hooks'
+// import reactRefresh from 'eslint-plugin-react-refresh'
 
-export default [
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-]
->>>>>>> origin/frontend/Sanghita
+// export default [
+//   { ignores: ['dist'] },
+//   {
+//     files: ['**/*.{js,jsx}'],
+//     languageOptions: {
+//       ecmaVersion: 2020,
+//       globals: globals.browser,
+//       parserOptions: {
+//         ecmaVersion: 'latest',
+//         ecmaFeatures: { jsx: true },
+//         sourceType: 'module',
+//       },
+//     },
+//     plugins: {
+//       'react-hooks': reactHooks,
+//       'react-refresh': reactRefresh,
+//     },
+//     rules: {
+//       ...js.configs.recommended.rules,
+//       ...reactHooks.configs.recommended.rules,
+//       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+//       'react-refresh/only-export-components': [
+//         'warn',
+//         { allowConstantExport: true },
+//       ],
+//     },
+//   },
+// ]
