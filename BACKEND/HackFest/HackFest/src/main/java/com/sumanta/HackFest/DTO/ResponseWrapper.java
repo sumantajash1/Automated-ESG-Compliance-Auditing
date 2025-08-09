5000e0aff78d2b0ee7cbe064cb2cce96c175db3f
sumanta <sumantajashh@gmail.com>
2025-08-09 20:37:32 +0530

    Configured ResponseWrapper Class

--- BACKEND/HackFest/HackFest/src/main/java/com/sumanta/HackFest/DTO/ResponseWrapper.java
+++ BACKEND/HackFest/HackFest/src/main/java/com/sumanta/HackFest/DTO/ResponseWrapper.java
@@ -1,1 +1,18 @@
+package com.sumanta.HackFest.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class ResponseWrapper<T> {
+    boolean success;
+    int httpStatusCode;
+    String message;
+    T data;
+    String error;
+}
