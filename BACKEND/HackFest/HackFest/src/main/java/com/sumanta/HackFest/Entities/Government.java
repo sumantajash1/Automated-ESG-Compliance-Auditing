bb506ef03a941a364b6e3044de310078a605b3cd
sumanta <sumantajashh@gmail.com>
2025-09-05 12:02:42 +0530

    Removed all the constructors and getter setters and toString method from Government class

--- BACKEND/HackFest/HackFest/src/main/java/com/sumanta/HackFest/Entities/Government.java
+++ BACKEND/HackFest/HackFest/src/main/java/com/sumanta/HackFest/Entities/Government.java
@@ -1,8 +1,16 @@
 package com.sumanta.HackFest.Entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 @Table(name = "government")
 public class Government {
     @Id
@@ -19,76 +27,4 @@ public class Government {
     @Column(name = "role")
     @Enumerated(EnumType.STRING)
     Role role = Role.GOVERNMENT;
-
-    public Government() {
-    }
-
-    public Government(String employeeId, String employeeName, String email, String contactNumber, String password, Role role) {
-        this.employeeId = employeeId;
-        this.employeeName = employeeName;
-        this.email = email;
-        this.contactNumber = contactNumber;
-        this.password = password;
-        this.role = role;
-    }
-
-    public String getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(String employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getEmployeeName() {
-        return employeeName;
-    }
-
-    public void setEmployeeName(String employeeName) {
-        this.employeeName = employeeName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getContactNumber() {
-        return contactNumber;
-    }
-
-    public void setContactNumber(String contactNumber) {
-        this.contactNumber = contactNumber;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    @Override
-    public String toString() {
-        return "Government{" +
-                "employeeId='" + employeeId + '\'' +
-                ", employeeName='" + employeeName + '\'' +
-                ", email='" + email + '\'' +
-                ", contactNumber='" + contactNumber + '\'' +
-                ", password='" + password + '\'' +
-                ", role=" + role +
-                '}';
-    }
 }
