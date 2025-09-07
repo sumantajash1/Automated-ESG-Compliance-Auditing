0d639641c573d49a63dfd43b71cbea56c0a386ee
sumanta <sumantajashh@gmail.com>
2025-09-07 18:28:49 +0530

    Added lombok annotations and removed getter setters and constrcutors of ClientDto.java

--- BACKEND/HackFest/HackFest/src/main/java/com/sumanta/HackFest/DTO/ClientDto.java
+++ BACKEND/HackFest/HackFest/src/main/java/com/sumanta/HackFest/DTO/ClientDto.java
@@ -1,63 +1,18 @@
 package com.sumanta.HackFest.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.stereotype.Component;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ClientDto {
     private String clientId;
     private String clientName;
     private String email;
     private String contact;
-
-    public ClientDto() {
-    }
-
-    public ClientDto(String clientId, String clientName, String email, String contact) {
-        this.clientId = clientId;
-        this.clientName = clientName;
-        this.email = email;
-        this.contact = contact;
-    }
-
-    public String getClientId() {
-        return clientId;
-    }
-
-    public void setClientId(String clientId) {
-        this.clientId = clientId;
-    }
-
-    public String getClientName() {
-        return clientName;
-    }
-
-    public void setClientName(String clientName) {
-        this.clientName = clientName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
-
-    @Override
-    public String toString() {
-        return "ClientDto{" +
-                "clientId='" + clientId + '\'' +
-                ", clientName='" + clientName + '\'' +
-                ", email='" + email + '\'' +
-                ", contact='" + contact + '\'' +
-                '}';
-    }
 }
