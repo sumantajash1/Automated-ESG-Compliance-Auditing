699e93a00e39454e045bc51707259fde51612fc5
sumanta <sumantajashh@gmail.com>
2025-09-10 01:00:44 +0530

    replaced constructors and getter setters with lombok annotation, and removed @Component annotation from SupplierDto class

--- BACKEND/HackFest/HackFest/src/main/java/com/sumanta/HackFest/DTO/SupplierDto.java
+++ BACKEND/HackFest/HackFest/src/main/java/com/sumanta/HackFest/DTO/SupplierDto.java
@@ -1,63 +1,15 @@
 package com.sumanta.HackFest.DTO;
 
-import org.springframework.stereotype.Component;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
 
-@Component
+@AllArgsConstructor
+@Data
+@ToString
 public class SupplierDto {
     private String gstNumber;
     private String supplierName;
     private String email;
     private String contactNumber;
-
-    public SupplierDto() {
-    }
-
-    public SupplierDto(String gstNumber, String supplierName, String email, String suppliername) {
-        this.contactNumber = suppliername;
-        this.gstNumber = gstNumber;
-        this.supplierName = supplierName;
-        this.email = email;
-    }
-
-    public String getGstNumber() {
-        return gstNumber;
-    }
-
-    public void setGstNumber(String gstNumber) {
-        this.gstNumber = gstNumber;
-    }
-
-    public String getSupplierName() {
-        return supplierName;
-    }
-
-    public void setSupplierName(String supplierName) {
-        this.supplierName = supplierName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSupplierEmail() {
-        return email;
-    }
-
-    public void setSupplierEmail(String suppliername) {
-        this.supplierName = suppliername;
-    }
-
-    @Override
-    public String toString() {
-        return "SupplierDto{" +
-                "gstNumber='" + gstNumber + '\'' +
-                ", supplierName='" + supplierName + '\'' +
-                ", email='" + email + '\'' +
-                ", contactNumber='" + contactNumber + '\'' +
-                '}';
-    }
 }
