ca89c0f2449f5ae66bb3a30b41f7df03faadab57
sumanta <sumantajashh@gmail.com>
2025-09-14 03:53:12 +0530

    replaced constructors and getter setters with lombok annotation, and removed @Component annotation from Supplier Entity class

--- BACKEND/HackFest/HackFest/src/main/java/com/sumanta/HackFest/Entities/Supplier.java
+++ BACKEND/HackFest/HackFest/src/main/java/com/sumanta/HackFest/Entities/Supplier.java
@@ -1,11 +1,19 @@
 package com.sumanta.HackFest.Entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.UUID;
 
 @Entity
 @Table(name="suppliers")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class Supplier {
     @Id
     @Column(name = "gstnumber")
@@ -21,76 +29,4 @@ public class Supplier {
     @Enumerated(EnumType.STRING)
     @Column(name="role")
     private Role role = Role.SUPPLIER;
-
-    public Supplier() {
-    }
-
-    public Supplier(String gstNumber, String supplierName, String email, String contactNumber, String password, Role role) {
-        this.gstNumber = gstNumber;
-        this.supplierName = supplierName;
-        this.email = email;
-        this.contactNumber = contactNumber;
-        this.password = password;
-        this.role = role;
-    }
-
-    public String getGstNumber() {
-        return gstNumber;
-    }
-
-    public void setGstNumber(String gstNumber) {
-        this.gstNumber = gstNumber;
-    }
-
-    public String getSupplierName() {
-        return supplierName;
-    }
-
-    public void setSupplierName(String supplierName) {
-        this.supplierName = supplierName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getContactNumber() {
-        return contactNumber;
-    }
-
-    public void setContactNumber(String contactNumber) {
-        this.contactNumber = contactNumber;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Role getRole() {
-        return role;
-    }
-
-    public void setRole(Role role) {
-        this.role = role;
-    }
-
-    @Override
-    public String toString() {
-        return "Supplier{" +
-                "gstNumber='" + gstNumber + '\'' +
-                ", supplierName='" + supplierName + '\'' +
-                ", email='" + email + '\'' +
-                ", contactNumber='" + contactNumber + '\'' +
-                ", password='" + password + '\'' +
-                ", role=" + role +
-                '}';
-    }
 }
